81a706de1ad2a43a8df5678b2faef8c5b67ddeaa
ShannStewart <Zackry.Broodie.Stewart@gmail.com>
2020-06-04 00:28:37 -0400

    made store variable

--- App.js
+++ App.js
@@ -3,9 +3,18 @@ import React from 'react';
 //import List from './composition/List';
 
 function App(){
+    const store =  (
+        <ul>
+          {[
+           <li key={1} />,
+            <li key={2} />,
+            <li key={3} />,
+          ]}
+        </ul>
+      )      
     return(
-    <main>
-        Stuff
+    <main className='App'>
+        {store}
     </main>
     )
 }
